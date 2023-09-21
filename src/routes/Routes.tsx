69dfe2a28213b0edad799c25bb707b22a0c6b5ad
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-21 19:32:36 +0500

    Update Routes.tsx

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -13,29 +13,29 @@ import Database from "../pages/Database";
 import DefaultInputs from "../pages/DefaultInputs";
 
 const AppLayout = ()=>{
-    return(
-      <div className="grid grid-cols-5 bg-white/60 backdrop-blur">
-        <div className="col-span-1">
-            <Sidebar/>
-        </div>
-        <div className="col-span-4 px-4 py-6 bg-gray-100">
-            <Outlet/>
-        </div>
+  return(
+    <div className="grid grid-cols-5 bg-white/60 backdrop-blur">
+      <div className="col-span-1">
+          <Sidebar/>
       </div>
-    )
-  }
+      <div className="col-span-4 px-4 py-6 bg-gray-100">
+          <Outlet/>
+      </div>
+    </div>
+  )
+}
 
-  export const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-      <Route path="/sign-in" element={<SignIn />}/>
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/*" element={<AppLayout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="scripts" element={<Scripts/>}/>
-        <Route path="database" element={<Database/>}/>
-        <Route path="default-inputs" element={<DefaultInputs/>}/>
-      </Route>
-      </>
-    )
-    );
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/sign-in" element={<SignIn />}/>
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/*" element={<AppLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="scripts" element={<Scripts/>}/>
+      <Route path="database" element={<Database/>}/>
+      <Route path="default-inputs" element={<DefaultInputs/>}/>
+    </Route>
+    </>
+  )
+);

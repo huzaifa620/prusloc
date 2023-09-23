3e56874b448c1c061b4d5fbbcab0eeeba4df8048
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-23 17:37:46 +0500

    Update DefaultInputs.tsx

--- src/pages/DefaultInputs.tsx
+++ src/pages/DefaultInputs.tsx
@@ -1,10 +1,36 @@
 import TextHeader from "../components/TextHeader";
+import { useEffect, useState } from 'react';
 
+function LiveLog() {
+  const [logData, setLogData] = useState('');
 
-export default function DefaultInputs() {
+  useEffect(() => {
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_FLASK_BASE_URL}/api/live-log`);
+
+    eventSource.onmessage = (event) => {
+      setLogData((prevData) => prevData + event.data + '\n');
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <div className="app">
-        <TextHeader title="Default Inputs"/>
+    <div className="app flex flex-col">
+      <TextHeader title="Default Inputs"/>
+      <div className="flex flex-col p-4 space-y-4">
+        <h1>Live Log:</h1>
+        <textarea
+          className="w-full h-[90%] p-4 rounded-2xl border shadow-2xl text-justify"
+          value={logData}
+          rows={30}
+          readOnly
+          style={{ whiteSpace: 'pre-wrap' }}
+        />
+      </div>
     </div>
-  )
+  );
 }
+
+export default LiveLog;

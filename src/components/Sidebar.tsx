6f67edda4d5fe9c91aad325813b6f1bdc76cb7d3
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-23 22:46:52 +0500

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,56 @@
 import { links } from "../data/SidebarLinks";
-import {Link, useLocation} from "react-router-dom"
-
+import { Link, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
+
+  const user = {
+    username: "JohnDoe",
+  };
+
+  const logout = () => {
+    
+  };
+
   return (
-    <div className="px-6 py-4 h-full text-primary border-r border-gray-200">
-        <Link to='/' className="text-2xl font-bold uppercase cursor-pointer">prusloc</Link>
+    <div className="px-6 py-4 h-full text-primary border-r border-gray-200 flex flex-col justify-between">
+
+      <div>
+        <Link to="/" className="text-2xl font-bold uppercase cursor-pointer">
+          prusloc
+        </Link>
+
         <div className="my-8">
-            {links?.map((link)=>{
-              const styles = pathname === link.href ? 'bg-primary text-white hover:bg-primary':'hover:bg-violet-50'
-              return(
-                <div className={`w-full group ${styles} rounded-md`}>
-                    <Link to={link.href} className={`w-full h-full py-2.5 px-3 my-2 font-semibold flex items-center group-hover:translate-x-2 duration-500 gap-2 `}>{link.icon}&nbsp;{link.title}</Link>
-                </div>
-              )
-            })}
+          {links?.map((link) => {
+            const styles =
+              pathname === link.href ? "bg-primary text-white hover:bg-primary" : "hover:bg-violet-50";
+            return (
+              <div
+                className={`w-full group ${styles} rounded-md`}
+                key={link.title}
+              >
+                <Link
+                  to={link.href}
+                  className={`w-full h-full py-2.5 px-3 my-2 font-semibold flex items-center group-hover:translate-x-2 duration-500 gap-2 `}
+                >
+                  {link.icon}&nbsp;{link.title}
+                </Link>
+              </div>
+            );
+          })}
         </div>
+      </div>
+
+      <div className="w-full flex items-center justify-between text-center space-x-4 bg-primary p-4 rounded-lg shadow-2xl border">
+        <div className="flex items-center justify-center space-x-4">
+          <div className="bg-sky-500 font-extrabold text-white py-2 px-4 rounded-xl"> {user.username[0]} </div>
+          <p className="text-white font-bold"> {user.username} </p>
+        </div>
+        <button onClick={logout} className="text-4xl cursor-pointer">
+          ↪️
+        </button>
+      </div>
+
     </div>
-  )
+  );
 }

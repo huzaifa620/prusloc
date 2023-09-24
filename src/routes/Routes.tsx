bdc41487c32125bde3704adac645a8d74cc7da8c
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-24 19:46:05 +0500

    Update Routes.tsx

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, createRoutesFromElements, Route, Outlet } from "react-router-dom";
+
+import { createBrowserRouter, createRoutesFromElements, Route, Outlet, Navigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import SignIn from "../pages/SignIn";
 import Home from "../pages/Home";
@@ -6,28 +7,42 @@ import Scripts from "../pages/Scripts";
 import Database from "../pages/Database";
 import LiveLog from "../pages/LiveLog";
 
-const AppLayout = ()=>{
-  return(
+export function checkToken() {
+  // Replace this logic with your actual token checking mechanism
+  const token = localStorage.getItem("token"); // You may store your token in localStorage
+  return !!token; // Return true if token exists, false otherwise
+}
+
+const AppLayout = () => {
+  // Check for the token
+  const hasToken = checkToken();
+
+  // If no token, redirect to sign-in page
+  if (!hasToken) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  return (
     <div className="grid grid-cols-5 bg-white/60 backdrop-blur">
       <div className="col-span-1">
-          <Sidebar/>
+        <Sidebar />
       </div>
       <div className="col-span-4 px-4 py-6 bg-gray-100">
-          <Outlet/>
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path="/*" element={<AppLayout />}>
-        <Route index element={<Home />}/>
-        <Route path="scripts" element={<Scripts />}/>
-        <Route path="database" element={<Database />}/>
-        <Route path="live-logs" element={<LiveLog />}/>
+        <Route index element={<Home />} />
+        <Route path="scripts" element={<Scripts />} />
+        <Route path="database" element={<Database />} />
+        <Route path="live-logs" element={<LiveLog />} />
       </Route>
     </>
   )

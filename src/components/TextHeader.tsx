4964e5710671fecd6ac43469557d6b7721e4ceb5
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-26 02:00:00 +0500

    icon added

--- src/components/TextHeader.tsx
+++ src/components/TextHeader.tsx
@@ -6,7 +6,7 @@ export default function TextHeader({title}:TextHeaderProps) {
     const currentLink = links.filter((link)=>link.title.toLowerCase() === title.toLowerCase())
 
   return (
-    <h2 className="font-bold text-xl ml-4 text-[#30415b] flex gap-2 items-center">{currentLink[0].icon}{title}</h2>
+    <h2 className="font-bold text-xl lg:ml-4 justify-center lg:justify-start text-[#30415b] flex gap-2 items-center">{currentLink[0].icon}{title}</h2>
   )
 }
 
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, createRoutesFromElements, Route, Outlet, Navigate } from "react-router-dom";
+import { MenuIcon } from "@heroicons/react/solid";
 import Sidebar from "../components/Sidebar";
 import SignIn from "../pages/SignIn";
 import Home from "../pages/Home";
@@ -19,9 +20,15 @@ const AppLayout = () => {
 
   return (
     <div className="grid grid-cols-4 lg:grid-cols-5 bg-white/60 backdrop-blur">
+      
       <div className="hidden lg:grid col-span-1">
         <Sidebar />
       </div>
+
+      <div className="fixed top-0 left-0 p-6 cursor-pointer lg:hidden">
+        <MenuIcon className="h-8 w-8 text-primary" />
+      </div>
+
       <div className="col-span-4 px-4 py-6 bg-gray-100">
         <Outlet />
       </div>

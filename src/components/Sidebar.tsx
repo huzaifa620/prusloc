334e16d2d62f1d5e634a294f3e895736526d44f6
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-26 23:20:49 +0500

    final updates

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -54,8 +54,10 @@ export default function Sidebar() {
               <div className="bg-sky-500 font-extrabold text-white py-2 px-3 2xl:px-4 uppercase rounded-xl"> {userName[0]} </div>
               <p className="text-white font-bold"> {userName} </p>
             </div>
-            <button onClick={logout} className="text-2xl 2xl:text-4xl cursor-pointer" title="Logout">
-              ↪️
+            <button onClick={logout} className="text-2xl 2xl:text-4xl hover:opacity-50 cursor-pointer" title="Logout">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+              </svg>
             </button>
           </div>
         )
